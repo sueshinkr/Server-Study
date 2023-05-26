7dce6d1bce44bf5c94e87c4631bb69549c4a306f
sueshinkr <100945798+sueshinkr@users.noreply.github.com>
2023-05-26 16:18:00 +0900

    Delete WebApplication1 directory

--- WebApplication1/Services/IGameDb.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace APIServer.Services;
-public interface IGameDb
-{
-    // 추후 기능 추가 예정
-    public Task<ErrorCode> CreateAccount(string email, string password);
-    public Task<Tuple<ErrorCode, Int64>> VerifyAccount(string email, string password);
-}
--- WebApplication1/Services/IRedisDb.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace APIServer.Services;
-public interface IRedisDb
-{
-    // 추후 기능 추가 예정
-    public Task<ErrorCode> RegistUser(string email, string authToken, Int64 accountid);
-}
